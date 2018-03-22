9891522d423a281b8de0a89af47693cba1ac7c47
Miquel <rossellomelismiquel@gmail.com>
2018-03-22 12:39:29 +0100

    Base Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class Controller {
+
+    @FXML
+    private TextField inputUser;
+    private TextField inputPassword;
+    private Label labelUser;
+    private Label labelPassword;
+    private Button loginButton;
+
+    @FXML
+    private void doLogin(ActionEvent evento) {
+        String username = inputUser.getText();
+        String password = inputPassword.getText();
+
+        System.out.println("Usuario: " + username + " Contraseña: " + password);
+    }
 }
